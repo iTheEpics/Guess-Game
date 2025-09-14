28aef86ab96d58a5e500d6a6dcdb0f33b9b56a34
iTheEpics <ihsanegebolgesi@gmail.com>
2025-09-14 19:21:44 +0200

    Guessing Game

---
+++ guess.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+
+int main(){
+
+    int secretNumber = 9;
+    int guess;
+    int attempt = 0;
+    int maxAttempts = 5;
+
+    while (guess != secretNumber)
+    {
+        if (attempt == maxAttempts)
+        {
+            printf("You have reached max attempts!\n");
+            return 1;
+        }
+        
+        printf("Guess a number: ");
+        scanf("%d", &guess);
+        attempt ++;
+
+        
+    }
+    printf("You win!\n");
+
+    
+
+
+    return 0;
+}
